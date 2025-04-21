3507e28c96294783c444d7121668d7bf8f9254af
dcwong <donald33wang@gmail.com>
2025-04-21 12:50:45 +0000

    Update pingpong & make it close pipes

--- user/pingpong.c
+++ user/pingpong.c
@@ -2,39 +2,46 @@
 #include "user/user.h"
 
 int main(int argc, char *argv[]) {
-  int pipe_p[2]; // for parent
-  int pipe_c[2]; // for child
-  if (pipe(pipe_p) < 0) {
-    fprintf(2, "pipe parent error.");
+  int parent_fd[2]; // for parent to send data to child
+  int child_fd[2]; // for child to send data to parent
+  if (pipe(parent_fd) < 0) {
+    fprintf(2, "parent pipe error\n");
     exit(1);
   }
-  if (pipe(pipe_c) < 0) {
-    fprintf(2, "pipe child error.");
+  if (pipe(child_fd) < 0) {
+    fprintf(2, "child pipe error\n");
     exit(1);
   }
 
   int pid = fork();
   if (pid > 0) {
-    close(pipe_c[1]); // unused
-
-    close(pipe_p[0]);
-    write(pipe_p[1], "x", 1);
+    // parent write should close the read end first
+    close(parent_fd[0]);
+    write(parent_fd[1], "x", 1);
+    close(parent_fd[1]); // close the write end after use
 
     wait((int *)0);
 
+    // parent read
+    close(child_fd[1]);
     char buf1[1];
-    read(pipe_c[0], buf1, sizeof(buf1));
+    read(child_fd[0], buf1, sizeof(buf1));
+    close(parent_fd[0]);
 
     printf("%d: received pong\n", getpid());
   } else if (pid == 0) {
-    close(pipe_p[1]); // unused
-    close(pipe_c[0]);
-
+    // child read should close the write end
+    close(parent_fd[1]);
     char buf2[1];
-    read(pipe_p[0], buf2, sizeof(buf2));
+    read(parent_fd[0], buf2, sizeof(buf2));
+    close(parent_fd[0]);
+
     printf("%d: received ping\n", getpid());
 
-    write(pipe_c[1], buf2, 1);
+    // child write
+    close(child_fd[0]);
+    write(child_fd[1], buf2, 1);
+    close(child_fd[1]);
   } else {
     fprintf(2, "Fork failed\n");
     exit(1);
